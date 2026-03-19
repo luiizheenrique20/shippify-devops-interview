899a35fa32786f98913eca5b523e2bc86436d480
Luiz Hossi <luiz.hossi.ext@niointernet.com.br>
2026-03-19 15:55:17 -0300

    ajuste lambda\dockerfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -197,9 +197,9 @@ resource "aws_lambda_function" "main" {
   tags = { Name = local.name }
 }
 
-#--
+# ---------------------------------------------------------------------------
 # IAM-POLICY
-#---
+# ---------------------------------------------------------------------------
 resource "aws_iam_role_policy" "lambda_s3" {
   name = "${local.lambda_role_name}-s3"
   role = aws_iam_role.lambda.id
@@ -233,4 +233,32 @@ resource "aws_iam_role_policy" "lambda_s3" {
       }
     ]
   })
+}
+
+# ---------------------------------------------------------------------------
+# NAT GATEWAY
+# ---------------------------------------------------------------------------
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${local.name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "${local.name}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
